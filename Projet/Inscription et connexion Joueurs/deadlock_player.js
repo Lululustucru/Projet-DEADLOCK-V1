4ed8fa7cc2129bd873afb82a385bad90fc78cab9
Lululustucru <cass.luca2606@gmail.com>
2025-11-04 13:33:29 +0100

    ajout des fichier

---
+++ Projet/Inscription et connexion Joueurs/deadlock_player.js
@@ -0,0 +1,4 @@
+const btn1 = document.getElementById("button");
+btn1.addEventListener('click', () =>{
+    
+})
---
+++ TP1.js
@@ -0,0 +1,8 @@
+let text = document.createElement('p')
+text.textContent = "je suis un message"
+
+let button = document.createElement('button')
+button.textContent = "afficher"
+
+document.body.append(text)
+document.body.appendChild(button)
